0945aa3d3e8d25bacf3a7506a8e675212d3633e2
Sequømics <sequomics@gmail.com>
2015-12-11 21:48:29 +0530

    Initial commit
    authors of EarthShuttle®.

--- source/core/fonts/compare.py
+++ source/core/fonts/compare.py
@@ -5,3 +5,7 @@ __company__ 	= 'Sequømics Corporation'
 __homepage__ 	= 'http://sequomics.com/'
 __account__ 	= 'SequomicsCorporation'
 __githubURL__ 	= 'https://github.com/SequomicsCorporation'
+__authors__ 	= [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
