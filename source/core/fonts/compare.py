a17a80676ceae88889be3a3a3068fc61b4a61c9e
Sequømics <sequomics@gmail.com>
2015-12-11 21:46:46 +0530

    Initial commit
    metadata of EarthShuttle®.

--- source/core/fonts/compare.py
+++ source/core/fonts/compare.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python
+
+
+__company__ 	= 'Sequømics Corporation'
+__homepage__ 	= 'http://sequomics.com/'
+__account__ 	= 'SequomicsCorporation'
+__githubURL__ 	= 'https://github.com/SequomicsCorporation'
